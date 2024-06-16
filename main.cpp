abedcde15667943cd6ff5300fe06303a185ad412
Muhammad Azeem <mailmeatazeem@gmail.com>
2024-06-16 13:19:36 +0500

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+# Code
+
+class Solution 
+{
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if(nums[i] == target - nums[j])
+                    return {i, j};
+            }
+        }
+        return {};
+    }
+};
